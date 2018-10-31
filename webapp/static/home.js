0bf8a708da3d8d76e3c72266a1a817528b1083b0
Addison Partida <CS@eduroam-174-155.dyn.carleton.edu>
2018-10-30 22:15:02 -0500

    updates

--- webapp/static/home.js
+++ webapp/static/home.js
@@ -1,3 +1,5 @@
+initialize();
+
 function initialize(){
   var element = document.getElementById('generateTable')
   if (element) {
@@ -8,7 +10,7 @@ function initialize(){
 
 var slider = document.getElementById("myRange");
 var output = document.getElementById("demo");
-output.innerHTML = slider.value; // Display the default slider value
+output.innerHTML = 15; // Display the default slider value
 
 // Update the current slider value (each time you drag the slider handle)
 slider.oninput = function() {
@@ -31,13 +33,12 @@ function onGenerateTableClicked() {
     .then((response) => response.json())
 
     // Once you have your list of author dictionaries, use it to build
+    console.log(response)
     // an HTML table displaying the author names and lifespan.
     .then(function(fooditemsList) {
         // Build the table body.
         var tableBody = '';
-        tableBody +=  '<div style="overflow-x:auto;">' +
-                      '<table id = "results_table">' +
-                      '<tr>' +
+        tableBody +=  '<tr>' +
                       '<th onclick="sortTable(0)">Item Name</th>' +
                       '<th onclick="sortTable(1)">Brand Name</th>' +
                       '<th onclick="sortTable(2)">Calories</th>' +
@@ -63,7 +64,7 @@ function onGenerateTableClicked() {
                       '</tr>' +
                       '</div>'
         for (var k = 0; k < fooditemsList.length; k++) {
-            tableBody = '<tr>''<td> + fooditemsList[k]['item_name']''</td>';
+            tableBody += '<tr>'+'<td>' + fooditemsList[k]['item_name']+'</td>';
             tableBody += '<td>'+ fooditemsList[k]['brand_name'] + '</td>';
             tableBody += '<td>'+ fooditemsList[k]['calories'] + '</td>';
             tableBody += '<td>'+ fooditemsList[k]['calories_fat'] + '</td>';
@@ -85,75 +86,19 @@ function onGenerateTableClicked() {
             tableBody += '<td>'+ fooditemsList[k]['potassium'] + '</td>';
             tableBody += '<td>'+ fooditemsList[k]['servings_per_cont'] + '</td>';
             tableBody += '<td>'+ fooditemsList[k]['serving_unit'] + '</td>';
-            tableBody'</tr>';
+            tableBody += '</tr>';
         }
         // Put the table body we just built inside the table that's already on the page.
         var resultsTableElement = document.getElementById('results_table');
         if (resultsTableElement) {
             resultsTableElement.innerHTML = tableBody;
         }
-    })
+    }
 
     // Log the error if anything went wrong during the fetch.
     .catch(function(error) {
         console.log(error);
-    });
+
+      }));
+
 }
-
-function sortTable(n) {
-  var table, rows, switching, i, x, y, shouldSwitch, dir, switchcount = 0;
-  table = document.getElementById("results_table");
-  switching = true;
-  //Set the sorting direction to ascending:
-  dir = "asc";
-  /*Make a loop that will continue until
-  no switching has been done:*/
-  while (switching) {
-    //start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    /*Loop through all table rows (except the
-    first, which contains table headers):*/
-    for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
-      shouldSwitch = false;
-      /*Get the two elements you want to compare,
-      one from current row and one from the next:*/
-      x = rows[i].getElementsByTagName("TD")[n];
-      y = rows[i + 1].getElementsByTagName("TD")[n];
-      /*check if the two rows should switch place,
-      based on the direction, asc or desc:*/
-      if (dir == "asc") {
-        if (x.innerHTML.toLowerCase() > y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch= true;
-          break;
-        }
-      } else if (dir == "desc") {
-        if (x.innerHTML.toLowerCase() < y.innerHTML.toLowerCase()) {
-          //if so, mark as a switch and break the loop:
-          shouldSwitch = true;
-          break;
-        }
-      }
-    }
-    if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
-      and mark that a switch has been done:*/
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-      //Each time a switch is done, increase this count by 1:
-      switchcount ++;
-    } else {
-      /*If no switching has been done AND the direction is "asc",
-      set the direction to "desc" and run the while loop again.*/
-      if (switchcount == 0 && dir == "asc") {
-        dir = "desc";
-        switching = true;
-      }
-    }
-  }
-}
-
-
-window.onload = initialize;
